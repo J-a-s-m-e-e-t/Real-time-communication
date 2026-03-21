dc87419fe3c743a821ccb9e5526d537f8ee84d6a
J-a-s-m-e-e-t <jasmeetsingh1613826@gmail.com>
2026-03-21 19:08:33 +0530

    update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const BASE_URL= import.meta.env.MODE==="development" ? "http://localhost:5001/api":"/api";
+const BASE_URL= import.meta.env.MODE==="development" ? "real-time-communication-production.up.railway.app":"/api";
  export const axiosInstance=axios.create({
     baseURL:BASE_URL,
     withCredentials:true, //send cookies
 
 })
+
+// "http://localhost:5001/api"
