785fb3293c3e17c4e38515e89aa9ba7dafc6d57c
J-a-s-m-e-e-t <jasmeetsingh1613826@gmail.com>
2026-03-21 15:49:18 +0530

    restored the changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ app.use("/api/users",userRoutes);
 app.use("/api/chat",chatRoutes);
 
 if(process.env.NODE_ENV ==="production"){
-    app.use(express.static(path.join(_dirname,"../frontend","dist")))
+    app.use(express.static(path.join(_dirname,"../frontend/dist")))
 }
 
 app.get("*",(req,res)=>{
