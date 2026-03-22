07247c49573477e0d3622f7061c67b9f12bb9bc7
J-a-s-m-e-e-t <jasmeetsingh1613826@gmail.com>
2026-03-22 20:17:46 +0530

    updates

--- frontend/src/component/Layout.jsx
+++ frontend/src/component/Layout.jsx
@@ -5,10 +5,10 @@ import Navbar from "./Navbar"
 
 const Layout = ({children,showSidebar=false}) => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-base-100">
         <div className="flex">
            {showSidebar && <Sidebar/>}
-           <div className="flex-1 flex flex-col">
+           <div className="flex-1 flex flex-col min-h-screen">
             <Navbar />
             <main className="flex-1 overflow-y-auto">{children}</main>
            </div>
