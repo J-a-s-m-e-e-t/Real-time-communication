33aedd2b96a91bc3665e56ef8ef7ff5f7d79feee
J-a-s-m-e-e-t <jasmeetsingh1613826@gmail.com>
2026-05-02 22:50:04 +0530

    port updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ import cors from "cors"
 import path from "path"
 
 const app=express();
-const PORT=process.env.PORT;
+const PORT=process.env.PORT || 5001;
 const _dirname=path.resolve();
 
 app.use(cors({
