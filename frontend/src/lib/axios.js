c4fe28ea50b7c8fe1387c6ba9981fcdeaaa3b7cb
J-a-s-m-e-e-t <jasmeetsingh1613826@gmail.com>
2026-05-02 21:25:52 +0530

    axios updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-// const BASE_URL= import.meta.env.MODE==="development" ?"http://localhost:5001/api":"/api";
-//  export const axiosInstance=axios.create({
-//     baseURL:`${import.meta.env.VITE_API_URL}/api`,
-//     withCredentials:true, //send cookies
+const BASE_URL= import.meta.env.MODE==="development" ?"http://localhost:5001/api":"/api";
+ export const axiosInstance=axios.create({
+    baseURL:`${import.meta.env.VITE_API_URL}/api`,
+    withCredentials:true, //send cookies
 
-// })
-const apiUrl = import.meta.env.VITE_API_URL;
+})
+// const apiUrl = import.meta.env.VITE_API_URL;
 
-if (import.meta.env.MODE === "production" && !apiUrl?.startsWith("http")) {
-  console.error("VITE_API_URL is missing or malformed:", apiUrl);
-}
+// if (import.meta.env.MODE === "production" && !apiUrl?.startsWith("http")) {
+//   console.error("VITE_API_URL is missing or malformed:", apiUrl);
+// }
 
-export const axiosInstance = axios.create({
-  baseURL: `${apiUrl}/api`,
-  withCredentials: true,
-});
+// export const axiosInstance = axios.create({
+//   baseURL: `${apiUrl}/api`,
+//   withCredentials: true,
+// });
