1e9f7e7bc069ce2d65041953e4cbf21685af6e90
J-a-s-m-e-e-t <jasmeetsingh1613826@gmail.com>
2026-05-03 21:59:58 +0530

    changes restored

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,13 +25,13 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/chat",chatRoutes);
 
-// if(process.env.NODE_ENV ==="production"){
-//     app.use(express.static(path.join(_dirname,"../frontend/dist")));
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(_dirname,"../frontend/dist")));
    
-//     app.get("*",(req,res)=>{
-//     res.sendFile(path.join(_dirname,"../frontend", "dist","index.html"));
-// })
-// }
+    app.get("*",(req,res)=>{
+    res.sendFile(path.join(_dirname,"../frontend", "dist","index.html"));
+})
+}
 
 
 
